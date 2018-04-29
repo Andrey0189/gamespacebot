4383d1502f4a6c350ef2c8c230e9957bcb059711
Vlad Ciphersky <97faizov@gmail.com>
2018-04-29 08:46:16 +0500

    Логгер (читать описание коммита)
    Логгер будет отправлять сообщения со всех каналов (которые ему доступны) в консоль

--- bot.js
+++ bot.js
@@ -268,6 +268,8 @@ client.on("guildMemberAdd", member => {
 });
 
 client.on("message", async message => {
+	// Небольшой логгер (?!), который будет отправлять сообщения, что пишут во всех каналах, доступных боту
+	console.log(`${message.author.tag} написал в канал "${message.channel.name}": ` + message.content);
     //Системные команды
     if (message.channel.id === '421260737281785856') {
         if(!message.author.bot) return;
