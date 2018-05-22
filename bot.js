83d5ccc08ea50a8709fa0e1ce002f7837abac903
zziger <zziger07@gmail.com>
2018-05-22 22:21:37 +0300

    done and active show

--- bot.js
+++ bot.js
@@ -1153,10 +1153,14 @@ client.on("message", async message => {
             request('http://' + process.env.SITE_DOMAIN + '/get_user_tasks.php?secret=' + encodeURIComponent(process.env.SECRET_KEY) + '&user=' + message.author.id, function (error, response, body) {
                 msg.edit(message.author + ', загляни в лс :D').then(msg => msg.delete(3000));
                 try {
+                    let done = '';
                     let tasks_data = JSON.parse(body);
                     tasks_data.forEach((item, num) => {
-                        if (item !== null)
-                            all[num] = ['***__' + item['name'] + '__***', newLines(item['task']).join('\n'), '🏆 Награда: **' + item['reward'] + '**' + money];
+                        if (item !== null) {
+                            if (item['active']) item['name'] = '*__' + item['name'] + '__*';
+                            if (item['active']) done = '✅ Выполнено: **'+item['done']+'**/**'+item['count']+'**'; else done = blank;
+                            all[num] = ['**' + item['name'] + '**', newLines(item['task']).join('\n'), '🏆 Награда: **' + item['reward'] + '**' + money, done];
+                        }
                         else
                             all[num] = [blank.toString(), 'Задания нет.\nПриходи завтра!', blank.toString()]
                     });
@@ -1174,9 +1178,9 @@ client.on("message", async message => {
                         embed: (new Discord.RichEmbed()
                                 .setColor('36393E')
                                 .setTitle(':bell: Ежедневные задания')
-                                .addField('Задание 1', `${all[0][0]}\n${blank}\n${all[0][1]}\n\n${blank}\n${blank}\n${all[0][2]}`, true)
-                                .addField('Задание 2', `${all[1][0]}\n${blank}\n${all[1][1]}\n${blank}\n${blank}\n${all[1][2]}`, true)
-                                .addField('Задание 3', `${all[2][0]}\n${blank}\n${all[2][1]}\n${blank}\n${blank}\n${all[2][2]}`, true)
+                                .addField('Задание 1', `${all[0][0]}\n${blank}\n${all[0][1]}\n\n${blank}\n${done}\n${all[0][2]}`, true)
+                                .addField('Задание 2', `${all[1][0]}\n${blank}\n${all[1][1]}\n${blank}\n${done}\n${all[1][2]}`, true)
+                                .addField('Задание 3', `${all[2][0]}\n${blank}\n${all[2][1]}\n${blank}\n${done}\n${all[2][2]}`, true)
                         )
                     });
                 } catch (e) {
