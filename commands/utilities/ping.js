771779b127a5eb69d038ad385e4fa66882b315be
zziger <zziger07@gmail.com>
2018-06-30 11:44:46 +0300

    owo

--- commands/utilities/ping.js
+++ commands/utilities/ping.js
@@ -10,13 +10,13 @@ module.exports.run = async function (client, message, command, args, info) {
 	const apiping = Math.round(client.ping)
     const embed = new Discord.RichEmbed()
         .setTitle('Пинг')
-        .setDescription(`\nОсновной сервер: Расчет...\nAPI сервер: ${apiping}мс`)
+        .setDescription(`\nОсновной сервер: **Расчет...**\nAPI сервер: **${apiping}**мс`)
         .setFooter('Внимание! Этот пинг отностится только к боту!')
         .setColor(color);
     message.channel.send({embed}).then(m => {
         const embed_req = new Discord.RichEmbed()
             .setTitle('Пинг')
-            .setDescription(`\nОсновной сервер: ${m.createdTimestamp - message.createdTimestamp}\nAPI сервер: ${apiping}мс`)
+            .setDescription(`\nОсновной сервер: **${m.createdTimestamp - message.createdTimestamp}**мс\nAPI сервер: **${apiping}**мс`)
             .setFooter('Внимание! Этот пинг отностится только к боту!')
             .setColor(color);
         m.edit({embed: embed_req});
