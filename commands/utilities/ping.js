4726942597c7f7d6b1a79de81786cd20022dd3ea
zziger <zziger07@gmail.com>
2018-06-30 11:41:49 +0300

    pinnngggg

--- commands/utilities/ping.js
+++ commands/utilities/ping.js
@@ -1,13 +1,16 @@
+const Discord = require('discord.js');
+const func = require('../../func.js');
+
 module.exports.info = {
 	command: '^(ping|п[и|і|i]нг)$'
 };
 module.exports.run = async function (client, message, command, args, info) {
 	message.delete();
-	const color = parseInt(getRandomInt(0, 16777214));
+	const color = parseInt(func.getRandomInt(0, 16777214));
 	const apiping = Math.round(client.ping)
     const embed = new Discord.RichEmbed()
         .setTitle('Пинг')
-        .setDescription(`\nОновной сервер: Расчет...\nAPI сервер: ${apiping}мс`)
+        .setDescription(`\nОсновной сервер: Расчет...\nAPI сервер: ${apiping}мс`)
         .setFooter('Внимание! Этот пинг отностится только к боту!')
         .setColor(color);
     message.channel.send({embed}).then(m => {
