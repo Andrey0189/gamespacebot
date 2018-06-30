cb0e9059bfaa54b42fd9ffe81badf2c376868181
zziger <zziger07@gmail.com>
2018-06-30 10:57:01 +0300

    yee

--- commands/moderation/test.js
+++ commands/moderation/test.js
@@ -1,5 +1,5 @@
 module.exports.info = {
-	command: 'testt?t?o?'
+	command: '^testt?t?o?$'
 };
 module.exports.run = async function (client, message, command, args, commandinfo) {
 message.channel.send(command);
