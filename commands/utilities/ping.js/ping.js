91ee48fc2c03acd7ecdba923e3e7f14756ed1bf2
zziger <zziger07@gmail.com>
2018-06-30 11:23:27 +0300

    Ping? Pong!

---
+++ commands/utilities/ping.js/ping.js
@@ -0,0 +1,21 @@
+module.exports.info = {
+	command: '^(ping|п[и|і|i]нг)$'
+};
+module.exports.run = async function (client, message, command, args, info) {
+	message.delete();
+	const color = parseInt(getRandomInt(0, 16777214));
+	const apiping = Math.round(client.ping)
+    const embed = new Discord.RichEmbed()
+        .setTitle('Пинг')
+        .setDescription(`\nОновной сервер: Расчет...\nAPI сервер: ${apiping}мс`)
+        .setFooter('Внимание! Этот пинг отностится только к боту!')
+        .setColor(color);
+    message.channel.send({embed}).then(m => {
+        const embed_req = new Discord.RichEmbed()
+            .setTitle('Пинг')
+            .setDescription(`\nОсновной сервер: ${m.createdTimestamp - message.createdTimestamp}\nAPI сервер: ${apiping}мс`)
+            .setFooter('Внимание! Этот пинг отностится только к боту!')
+            .setColor(color);
+        m.edit({embed: embed_req});
+    });
+}
