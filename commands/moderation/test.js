84499654bdf355570a77f687ec46612f0703363a
zziger <zziger07@gmail.com>
2018-07-02 17:40:28 +0300

    oh my god!

--- commands/moderation/test.js
+++ commands/moderation/test.js
@@ -1,4 +1,3 @@
-const request = require("request");
 module.exports.info = {
 	command: '^testt?t?o?$',
 	name: 'test',
@@ -18,10 +17,5 @@ module.exports.info = {
     }
 };
 module.exports.run = async function (client, message, command, args, commandinfo) {
-    message.delete();
-    let lang = args[0].replace(/([uу][aа]|укр?([ао]ин(ский|а))?|uk(ra?i?n?(e|ian)?)?)/im, 'ua').replace(/([rр][uу]|рус?с?к?и?й?|рос?с?и?я?|russ?ian?)/im, 'ru');
-    if (!lang.match(/([uу][aа]|[eеа][nн])/))
-    request('http://'+process.env.SITE_DOMAIN+'/set_lang.php?secret='+encodeURIComponent(process.env.SECRET_KEY)+'&user='+message.author.id, function (error, response, body) {
-
-    });
+    message.channel.send(command);
 };
--- commands/others/lang.js
+++ commands/others/lang.js
@@ -1,3 +1,5 @@
+const func = require('../../func.js');
+const request = require("request");
 module.exports.info = {
 	command: '^(lang(uage)?|язык|мова|j(ę|en?)zyk)$',
 	name: 'lang',
@@ -16,6 +18,53 @@ module.exports.info = {
         }
     }
 };
-module.exports.run = async function (client, message, command, args, commandinfo) {
-message.channel.send(command);
+let lang = {
+    'ru': {
+        'main_server': 'Основной сервер',
+        'api_server': 'API сервер',
+        'ms': 'мс',
+        'ping': 'Пинг',
+        'calculation': 'Расчет',
+        'only_bots': 'Внимание! Это пинг бота, а не Ваш!',
+    },
+    'ua': {
+        'main_server': 'Основний сервер',
+        'api_server': 'API сервер',
+        'ms': 'мс',
+        'ping': 'Пінг',
+        'calculation': 'Розрахунек',
+        'only_bots': 'Увага! Це пінг бота, а не Ваш!',
+    },
+    'en': {
+        'main_server': 'Main server',
+        'api_server': 'API server',
+        'ms': 'ms',
+        'ping': 'Ping',
+        'calculation': 'Calculation',
+        'only_bots': 'Attention! This is ping of bot, not yours!',
+    },
+    'pl': {
+        'main_server': 'Główny serwer',
+        'api_server': 'API serwer',
+        'ms': 'ms',
+        'ping': 'Ping',
+        'calculation': 'Obliczanie',
+        'only_bots': 'Uwaga, proszę! Ten ping bota, nie twój!',
+    }
+};
+module.exports.run = async function (client, message, command, args, info, language) {
+    message.delete();
+    let lang = '';
+    if (args[0].match(/([uу][aа]|укр?([ао]ин(ский|а))?|uk(ra?i?n?(e|ian)?)?|укра[їіi]н(ський|[аa]))/im)) lang = 'ua';
+    else if (args[0].match(/([rр][uу]|рус?с?к?и?й?|рос?с?и?я?|russ?ian?|р[оу]сс?[іi](йський|я))/im)) lang = 'ru';
+    else if (args[0].match(/([eеэа][nн]|англ?([иіi](я|йсь?кий))?|амер[иіi]кан(сь?кий)?|(вел[иіi]ко-? ?)?бр[иіi]тан(ия|сь?кий)|eng?(lish)?|american?|(great-? ?)?britain(ian)?|[iіиеэє]нгл[iіие][шж])/im)) lang = 'en';
+    else if (args[0].match(/([pп][lл]|pol(i(sh|sz))?|пол(иш|ь?ский|ян[дт]ский|ян[дт])?|polskiy?)/im)) lang = 'pl';
+    else return message.channel.send(func.generateErrorMessage());
+    request('http://'+process.env.SITE_DOMAIN+'/set_lang.php?secret='+encodeURIComponent(process.env.SECRET_KEY)+'&user='+message.author.id+'&lang='+lang, function (error, response, body) {
+        if (lang === 'ru') {
+            if (client.langs.has(message.author.id)) client.langs.delete(message.author.id);
+        } else {
+            client.langs.set(message.author.id, lang);
+        }
+    });
 };
