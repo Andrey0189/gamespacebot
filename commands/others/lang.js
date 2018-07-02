c912c5f96b6ae70643d0da3cab05d27f0251c999
zziger <zziger07@gmail.com>
2018-07-02 18:33:00 +0300

    fix?

--- commands/others/lang.js
+++ commands/others/lang.js
@@ -39,7 +39,6 @@ let lang_phrases = {
 };
 module.exports.run = async function (client, message, command, args, info, language) {
     message.delete();
-    let words = lang_phrases[language];
     let lang = '';
     args[0] = args[0].replace(/`/g, "`" + String.fromCharCode(8203));
     if (args[0].match(/([uу][aа]|укр?([ао]ин(ский|а))?|uk(ra?i?n?(e|ian)?)?|укра[їіi]н(ський|[аa]))/im)) lang = 'ua';
@@ -53,6 +52,7 @@ module.exports.run = async function (client, message, command, args, info, langu
         } else {
             client.langs.set(message.author.id, lang);
         }
+        let words = lang_phrases[lang];
         message.channel.send(new Discord.RichEmbed().setColor('77B255').setTitle(words['done']).setDescription(`✅ ${words['successful']}`))
     });
 };
