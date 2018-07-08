2406c8fd684bcb2b7666f2d494337ec2d3c3d68b
zziger <zziger07@gmail.com>
2018-07-08 10:10:21 +0300

    ohh

--- events/message/system_commands.js
+++ events/message/system_commands.js
@@ -9,12 +9,20 @@ module.exports.run = function (message) {
     const args = message.content.slice('.'.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
-    let lang = {
-        'ru': 'Для того, чтобы присоединится к этим людям, и войти в один голосовой канал с ними вам нужно нажать на эту ссылку:\n<',
-        'ua': 'Для того, щоб приєднається до цих людей, і увійти в один голосовий канал з ними вам потрібно натиснути на це посилання:\n<',
-        'en': 'In order to join these people, and to enter one voice channel with them you need to click on this link:\n<',
-        'pl': 'Aby dołączyć do tych osób i wprowadzić jeden kanał głosowy za ich pomocą, musisz kliknąć ten link:\n<',
+    let games = {
+        'rl': 'Rocket League',
+        'd2': 'Dota 2',
+        'cs': 'CS:GO',
+        'll': 'League of Legends'
     };
+
+    let lang = {
+        'ru': 'Для того, чтобы присоединится к игрокам в **'+games[args[0]]+'**, и войти в один голосовой канал с ними вам нужно нажать на эту ссылку:\n<',
+        'ua': 'Для того, щоб приєднається до гравців в **'+games[args[0]]+', і увійти в один голосовий канал з ними вам потрібно натиснути на це посилання:\n<',
+        'en': 'In order to join players of **'+games[args[0]]+'**, and to enter one voice channel with them you need to click on this link:\n<',
+        'pl': 'Aby dołączyć do graczy w **'+games[args[0]]+'** i wprowadzić jeden kanał głosowy za ich pomocą, musisz kliknąć ten link:\n<',
+    };
+
 
     let lang_code = message.client.langs.get(args[0]) || 'ru';
     lang = lang[lang_code];
