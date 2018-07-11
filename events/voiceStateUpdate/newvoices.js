4ff78bbc920b949b494d5d6940d8978a71993d17
zziger <zziger07@gmail.com>
2018-07-11 18:11:00 +0300

    oh

--- events/voiceStateUpdate/newvoices.js
+++ events/voiceStateUpdate/newvoices.js
@@ -1,5 +1,15 @@
 module.exports.run = function (old_m, new_m) {
+    new_m.client.channels.get('466602142774329345').children.filter(c => c.id !== '466601087072206858').forEach((channel, id) => {
+        if (channel.members.size === 0) {
+            setTimeout(() => {
+                if (channel.members.size === 0) {
+                    channel.delete();
+                }
+            }, 60000)
+        }
+    });
     if (new_m.voiceChannelID === '466601087072206858') {
+        if (new_m.roles.has('466621050340507649')) return new_m.setVoiceChannel(new_m.guild.afkChannelID);
         new_m.guild.createChannel('Созвездие '+new_m.displayName, 'voice', [{
             id: new_m.id,
             allow: ['MANAGE_CHANNELS']
