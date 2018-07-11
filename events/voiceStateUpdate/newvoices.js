c01a5584ae0dbd0e1a99d4f91b6a174f2d2fd854
zziger <zziger07@gmail.com>
2018-07-11 16:50:56 +0300

    rly?

---
+++ events/voiceStateUpdate/newvoices.js
@@ -0,0 +1,8 @@
+module.exports.run = function (old_m, new_m) {
+    if (new_m.voiceChannelID === '466601087072206858') {
+        new_m.guild.createChannel('Созвездие '+new_m.displayName, 'voice', [{
+            id: new_m.id,
+            allow: ['MANAGE_CHANNELS']
+        },{id: new_m.guild.id, deny: ['VIEW_CHANNEL']}]).then(channel => {channel.setParent('466602142774329345').catch();new_m.setVoiceChannel(channel.id).catch()})
+    }
+};
