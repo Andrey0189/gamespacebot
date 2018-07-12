2edceff3b3d79a64febfb016046c26bc99a74917
zziger <zziger07@gmail.com>
2018-07-12 21:05:53 +0300

    ahahah

--- events/messageReactionAdd/language.js
+++ events/messageReactionAdd/language.js
@@ -1,7 +1,7 @@
 module.exports.run = function (reaction, user) {
-    if (reaction.message.id !== '466113074512789524') return;
+    if (reaction.message.id !== '466113221464424459') return;
     console.log(reaction.emoji.name);
-    if (reaction.emoji.name !== ':ok:') return;
+    if (reaction.emoji.name !== '🆗') return;
     let react = reaction.message.reactions.filter(r => r.users.has(user.id));
     if (!react) reaction.remove(user);
     console.log(react);
